e3d3935c7d24b79ac14a930e72d08d701a6e9277
BOUDIER Nathanael <nathanael.boudier@gmail.com>
2024-01-23 11:05:14 +0100

    Creation de la connection et query pour l'utilisateur

---
+++ public/utilisateur.php
@@ -0,0 +1,51 @@
+<?php
+
+header("Content-Type: application/json");
+require_once "connect.php";
+
+// session_start();
+// if (!isset($_SESSION['user'])) {
+//     http_response_code(401);
+//     echo json_encode(['error' => 'User not logged in']);
+//     exit;
+// }
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    $request = $_GET['request'];
+    $reponse = [];
+
+    if ($request == "all"){
+        $query = "SELECT * FROM `utilisateur`";
+        $resultat_query = executerRequeteSelect($connexion,$query);
+        $data = $resultat_query['donnees'];
+        $echec = $resultat_query['echec'];
+        if ($echec) {
+            http_response_code(500);
+            echo json_encode(['error' => 'Internal Server Error']);
+            exit;
+        } else {
+            http_response_code(200);
+            echo json_encode($data);
+            exit;
+        }
+    }
+    if ($request == "one"){
+        $id = $_GET['id'];
+        $query = "SELECT * FROM `utilisateur` WHERE `numUtilisateur` = $id";
+        $resultat_query = executerRequeteSelect($connexion,$query);
+        $data = $resultat_query['donnees'];
+        $echec = $resultat_query['echec'];
+        if ($echec) {
+            http_response_code(500);
+            echo json_encode(['error' => 'Internal Server Error']);
+            exit;
+        } else {
+            http_response_code(200);
+            echo json_encode($data);
+            exit;
+        }
+    }
+}
+?>
