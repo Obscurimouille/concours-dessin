13405c1b746c994c1c43f3d1b07bdc30422c8ca3
florian_G <florian.gouverneur@reseau.eseo.fr>
2024-01-29 21:46:04 +0100

    SQL request last version

--- database/requete9.sql
+++ database/requete9.sql
@@ -1,9 +1,15 @@
 SELECT DISTINCT
 utilisateur.nom as 'nom de l_evaluateur',
+club.region,
 evaluateur.specialite,
 avg(evaluation.note) as 'note moyenne'
-from evaluation,evaluateur,utilisateur
+from evaluation,evaluateur,utilisateur,participationevaluateur,club
 WHERE evaluateur.numEvaluateur=utilisateur.numUtilisateur
 and evaluation.numEvaluateur=evaluateur.numEvaluateur
+and club.numClub=participationevaluateur.numConcours
+and evaluateur.numEvaluateur=participationevaluateur.numEvaluateur
+and club.region IN (
+    SELECT club.region FROM club where upper(club.region)='BRETAGNE'
+    )
 GROUP BY 'nom de l_evaluateur'
 ORDER BY 'note moyenne' ASC
